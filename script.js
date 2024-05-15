5637463b4c2d085b90a206a13c861671970b8642
“inna_tssss” <inna.tspin11@gmail.com>
2024-05-15 12:59:47 +0200

    scrolling animation + header button

--- script.js
+++ script.js
@@ -63,3 +63,20 @@ $(document).ready(function () {
     });
   });
 });
+
+// fade anuimation
+
+let elementsArray = document.querySelectorAll(".sec");
+window.addEventListener("scroll", fadeIn);
+function fadeIn() {
+  for (var i = 0; i < elementsArray.length; i++) {
+    var elem = elementsArray[i];
+    var distInView = elem.getBoundingClientRect().top - window.innerHeight + 40;
+    if (distInView < 0) {
+      elem.classList.add("inView");
+    } else {
+      elem.classList.remove("inView");
+    }
+  }
+}
+fadeIn();
